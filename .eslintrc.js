0d767aded68464a8c2d5eac68485920a123c6413
vodarum <muradovravshan@yandex.ru>
2022-08-17 20:54:18 +0300

    Add Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'max-len': [
+      'error', {
+        ignoreComments: true,
+      },
+    ],
+  },
+};
