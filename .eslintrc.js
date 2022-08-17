b9acb1c5525db53c8273a86faafa4084e2ab7d09
vodarum <muradovravshan@yandex.ru>
2022-08-17 21:30:18 +0300

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'jest/globals': true,
+    "jest/globals": true,
   },
-  extends: 'airbnb-base',
+  extends: ["airbnb-base", "prettier"],
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['jest'],
+  plugins: ["jest"],
   rules: {
-    'import/prefer-default-export': 'off',
-    'max-len': [
-      'error', {
+    "import/prefer-default-export": "off",
+    "max-len": [
+      "error",
+      {
         ignoreComments: true,
       },
     ],
